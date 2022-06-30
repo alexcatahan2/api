9e546de4d7a9c6bf78c11c4d7f6eaf33ae120612
Alex Catahan <rac4sq@virginia.edu>
2022-06-29 22:18:37 -0400

    updates

--- app/routers/post.py
+++ app/routers/post.py
@@ -12,7 +12,7 @@ router = APIRouter(
 
 
 @router.get("/", response_model=List[schemas.PostOut])
-def get_posts(db : Session = Depends(get_db), limit : int = 10, skip : int = 0, search:Optional[str] = ''):
+def get_posts(db : Session = Depends(get_db), current_user : int = Depends(oauth2.get_current_user), limit : int = 10, skip : int = 0, search:Optional[str] = ''):
   #posts = db.query(models.Post).filter(models.Post.title.contains(search)).limit(limit).offset(skip).all()
   #raw SQL query action:
   # cursor.execute("SELECT * FROM posts")
