1bd490b2311b2477369d083948a5a8b85e0c2c67
Alex Catahan <rac4sq@virginia.edu>
2022-07-04 00:24:55 -0400

    adding workout and exercises table to db

--- alembic/versions/6be693d1d22a_adding_workout_table.py
+++ alembic/versions/6be693d1d22a_adding_workout_table.py
@@ -23,7 +23,8 @@ def upgrade() -> None:
                     sa.Column('start', sa.TIMESTAMP(timezone=True), nullable = False, server_default = sa.text('now()')),
                     sa.Column('stop', sa.TIMESTAMP(timezone=True), nullable = False, server_default = sa.text('now()')), 
                     sa.Column('length', sa.Integer(), nullable = False, server_default= '0'),
-                    sa.Column('date', sa.Date(), nullable = False, server_default = sa.text('CURRENT_DATE')))
+                    sa.Column('date', sa.Date(), nullable = False, server_default = sa.text('CURRENT_DATE')),
+                    sa.Column('user_id', sa.Integer, nullable = False))
     op.create_table('exercises', 
                     sa.Column('exercise_id', sa.Integer(), primary_key = True, nullable = False),
                     sa.Column('workout_id', sa.Integer(), nullable = False), 
@@ -31,14 +32,18 @@ def upgrade() -> None:
                     sa.Column('repititions', sa.Integer(), nullable = False, server_default = '10'),
                     sa.Column('weight', sa.Integer(), nullable = False),
                     sa.Column('start', sa.TIMESTAMP(timezone=True), nullable = False, server_default= sa.text('now()')),
-                    sa.Column('stop', sa.TIMESTAMP(timezone=True), nullable = False, server_default= sa.text('now()')))
+                    sa.Column('stop', sa.TIMESTAMP(timezone=True), nullable = False, server_default= sa.text('now()')))   
+    op.create_foreign_key('workouts_users_fk', source_table='workouts', referent_table='users',
+                            local_cols=['users_id'], remote_cols=['id'], ondelete="CASCADE")
     op.create_foreign_key('exercises_workouts_fk', source_table='exercises', referent_table='workouts',
                             local_cols=['workout_id'], remote_cols=['id'], ondelete="CASCADE")
+ 
     pass
 
 
 
 def downgrade() -> None:
+    op.drop_constraint('workouts_users_fk', table_name='workouts')
     op.drop_constraint('exercises_workouts_fk', table_name='exercises')
     op.drop_table('workouts')
     op.drop_table('exercises')
